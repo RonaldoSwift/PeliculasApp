ff6fdf5496c49790a80371774707f22be8ac6070
Ronado Vargas <ronaldovh99@gmail.com>
2024-08-23 17:59:26 -0500

    Initial Commit

---
+++ PeliculasApp/PeliculasAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeliculasAppApp.swift
+//  PeliculasApp
+//
+//  Created by Ronaldo Andre on 23/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PeliculasAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
