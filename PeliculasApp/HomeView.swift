4ca6893ef029aa94af1d93c2f922554350f99370
Ronado Vargas <ronaldovh99@gmail.com>
2024-08-26 11:56:05 -0500

    Add Configuracion, Logo de la aplicacion

---
+++ PeliculasApp/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  PeliculasApp
+//
+//  Created by Ronaldo Andre on 23/08/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack{
+            Text("Hello, World!")
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
